f57cbf2f8f8e554b0147c9d45632eaab644f4a4b
Jess Graham <jgraham@dealerops.com>
2025-01-21 13:21:55 -0500

    Create sign in form and link

--- apps/web/src/features/auth/pages/sign-in/sign-in.tsx
+++ apps/web/src/features/auth/pages/sign-in/sign-in.tsx
@@ -1,8 +1,13 @@
 import { VStack } from '@chakra-ui/react';
 import { cx, dt } from '@src/utils';
+import SignInForm from './sign-in-form';
 
 const SignIn = () => {
-    return <VStack className={cx('sign-in')} data-testid={dt('sign-in')}></VStack>;
+    return (
+        <VStack className={cx('sign-in')} data-testid={dt('sign-in')}>
+            <SignInForm />
+        </VStack>
+    );
 };
 
 export default SignIn;
