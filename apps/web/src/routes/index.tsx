c15ba0daedf26f3615dfd45f54a068ef8a062c3d
Jess Graham <jgraham@dealerops.com>
2025-01-23 13:07:27 -0500

    Save new redirects for index pages

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,19 +1,23 @@
-import { App, Auth } from '@src/features';
-import SignIn from '@src/features/auth/pages/sign-in/sign-in';
-import SignUp from '@src/features/auth/pages/sign-up/sign-up';
 import { Navigate, Route, Routes } from 'react-router';
+import { App, Auth } from '@src/features';
+import { Dashboard } from '@src/features/app/pages';
+import { SignIn, SignUp } from '@src/features/auth/pages';
 
 const AppRoutes = () => {
     return (
         <Routes>
             {/* Public routes */}
             <Route path={'auth'} element={<Auth />}>
+                <Route index element={<Navigate to="sign-in" />} />
                 <Route path={'sign-in'} element={<SignIn />} />
                 <Route path={'sign-up'} element={<SignUp />} />
             </Route>
 
             {/* Protected routes */}
-            <Route path={'app'} element={<App />}></Route>
+            <Route path={'app'} element={<App />}>
+                <Route index element={<Navigate to="dashboard" />} />
+                <Route index path={'dashboard'} element={<Dashboard />} />
+            </Route>
 
             {/* Fallback route for unknown routes */}
             <Route path={'*'} element={<Navigate to="/auth/sign-in" />} />
